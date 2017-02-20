39c97bf0072947fb48ca353cf0e4fa47654bce39
Unknown <ngyaomin@ymail.com>
2017-02-20 20:38:48 +0800

    Create form to post pujas

--- guru/app/controllers/pujas_controller.rb
+++ guru/app/controllers/pujas_controller.rb
@@ -1,2 +1,4 @@
 class PujasController < ApplicationController
+  def new
+  end
 end
--- guru/config/routes.rb
+++ guru/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :pujas
+  get 'lineages/index'
+  root 'lineages#index'
 
-  root 'welcome#index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
