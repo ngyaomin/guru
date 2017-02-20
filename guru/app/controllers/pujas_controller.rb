39f8281492e3d40eb3b1f0606b3947fc0e334070
Unknown <ngyaomin@ymail.com>
2017-02-20 20:17:15 +0800

    Generate Controller Pujas

---
+++ guru/app/controllers/pujas_controller.rb
@@ -0,0 +1,2 @@
+class PujasController < ApplicationController
+end
---
+++ guru/test/controllers/pujas_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PujasControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
