34982a0a649f7fd6aa70513becdab90b29c8db07
Unknown <ngyaomin@ymail.com>
2017-02-21 20:40:31 +0800

    Define create function

--- guru/app/controllers/pujas_controller.rb
+++ guru/app/controllers/pujas_controller.rb
@@ -3,6 +3,15 @@ class PujasController < ApplicationController
   end
 
   def create
-    render plain: params[:puja].inspect
+  @puja = Puja.new(params[:puja])
+
+  @puja.save
+  redirect_to @puja
   end
+
+  private
+    def puja_params
+      params.require(:puja).permit(:name, :description, :benefit, :suggested_offering)
+    end
+
 end
