6c278d94f2d5ff8dff4c5f2e8c2d6ad4234f895c
Unknown <ngyaomin@ymail.com>
2017-02-24 21:33:35 +0800

    Migrate database

--- guru/db/schema.rb
+++ guru/db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170221123352) do
+ActiveRecord::Schema.define(version: 20170224133252) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "pictures", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "pujas", force: :cascade do |t|
