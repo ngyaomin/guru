0fde87915dc68442f11c7a008261cc2578ab4103
Unknown <ngyaomin@ymail.com>
2017-02-25 22:08:19 +0800

    Add OmmiAuth to user and migrate

--- guru/db/schema.rb
+++ guru/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170225090916) do
+ActiveRecord::Schema.define(version: 20170225140744) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
