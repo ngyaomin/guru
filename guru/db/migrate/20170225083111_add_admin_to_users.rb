d8692ef18824c42a5835147e1d529aee0e98563d
Unknown <ngyaomin@ymail.com>
2017-02-25 16:32:03 +0800

    Add admin to devise

--- guru/db/migrate/20170225083111_add_admin_to_users.rb
+++ guru/db/migrate/20170225083111_add_admin_to_users.rb
@@ -1,5 +1,7 @@
 class AddAdminToUsers < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :admin, :boolean
+    add_column :users, :default, :string
+    add_column :users, :false, :string
   end
 end
